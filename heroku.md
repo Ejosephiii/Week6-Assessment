2f15064705b315ead8c09e3b17c5105916e96e31
Ethan Winegardner <ejosephhiii@gmail.com>
2022-07-22 11:34:40 -0600

    update2

--- heroku.md
+++ heroku.md
@@ -1,2 +1,2 @@
 ## Paste your link below
-
+https://week6-assessment-ethan.herokuapp.com/
--- utils.js
+++ utils.js
@@ -1,4 +1,4 @@
-export function shuffleArray(array) {
+function shuffleArray(array) {
     let arrCopy = [...array]
     for (let i = arrCopy.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
